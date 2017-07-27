22ec5556601e8ce535360ef57a64396d58135f3b
pilarArr <pilararr@users.noreply.github.com>
2017-07-27 14:24:29 +0200

    BugFix
    Daba error al encontrar una linea FMT en la mitad de los mensajes de
    datos del log. Se ha cambiado el codigo para haga una primera lectura
    de todos los FMT y luego de los mensajes de log.

--- logProcesses/Log2varF.js
+++ logProcesses/Log2varF.js
@@ -3,17 +3,13 @@
  */
 
 
-
-var myLogPath = "../sample-logs/SRM/2017-02-21 17-24-06.log";
+var myLogPath = "../sample-logs/SRM/47 11-07-2017 14-16-04.bin.log";
 var lineReader = require("line-reader");
 var fs = require("fs");
-
-
 
 function log2Var(locLog,callback) {
 
     var myLogVar = {};
-
 
 
     function parseFMT(logVar, arraydeLinea){
@@ -26,14 +22,14 @@ function log2Var(locLog,callback) {
             logVar.FMT.push({});
         }
         var columns = ["Type","Length","Name","Format","Columns"];
-        for (let i = 1 ; i < arraydeLinea.length ; i++) {
+        for (var i = 1 ; i < arraydeLinea.length ; i++) {
             if(i < 5){
                 logVar.FMT[logVar.FMT.length-1][columns[i-1]] = arraydeLinea[i];
             }
             else if(i == 5){
                 logVar.FMT[logVar.FMT.length-1][columns[i-1]] = [];
                 var mycols = arraydeLinea[i].split(",");
-                for(let j = 0; j < mycols.length; j++){
+                for(var j = 0; j < mycols.length; j++){
                     logVar.FMT[logVar.FMT.length-1][columns[i-1]].push(mycols[j]);
                 }
             }
@@ -56,14 +52,14 @@ function log2Var(locLog,callback) {
 
         // Search for the FMT value to get columns
         //TODO try to make this bisection search after a merge-sort of the FMT array by name to make the search faster
-        for (let i = 0; i < logVar.FMT.length; i++) {
+        for (var i = 0; i < logVar.FMT.length; i++) {
             if (logVar.FMT[i].Name == arraydeLinea[0]) {
                 var columns = logVar.FMT[i].Columns;
                 break;
             }
         }
         // Parse the columns
-        for (let i = 1; i < arraydeLinea.length; i++) {
+        for (var i = 1; i < arraydeLinea.length; i++) {
             if (isNaN(Number(arraydeLinea[i]))) {
                 logVar[arraydeLinea[0]][logVar[arraydeLinea[0]].length - 1][columns[i - 1]] = arraydeLinea[i];
             }
@@ -78,12 +74,12 @@ function log2Var(locLog,callback) {
 
     lineReader.eachLine(locLog, function (line, last) {
 
-        let lineArr = line.split(", ");
+        var lineArr = line.split(", ");
 
-        if (lineArr[0] == "FMT") {
+        if (lineArr[0] === "FMT") {
             parseFMT(myLogVar, lineArr);
         }
-        else {
+        else if (last) {
             console.log("Fin FMT");
             return false;
         }
@@ -93,9 +89,9 @@ function log2Var(locLog,callback) {
 
     lineReader.eachLine(locLog, function (line, last) {
 
-        let lineArr = line.split(", ");
+        var lineArr = line.split(", ");
 
-        if (lineArr[0] != "FMT") {
+        if (lineArr[0] !== "FMT") {
             parseMSG(myLogVar, lineArr);
         }
         if (last) {
@@ -111,7 +107,7 @@ function log2Var(locLog,callback) {
 // Esta funcion la uso para formatear strings como en java
 if (!String.format) {
     String.format = function(format) {
-        let args = Array.prototype.slice.call(arguments, 1);
+        var args = Array.prototype.slice.call(arguments, 1);
         return format.replace(/{(\d+)}/g, function(match, number) {
             return typeof args[number] != 'undefined'
                 ? args[number]
@@ -125,22 +121,22 @@ if (!String.format) {
 function printObj2JSON(myLogVar){
     var result = '{\n';
     var separ1 = "";
-    for (let key1 in myLogVar){
+    for (var key1 in myLogVar){
         if (key1 == "FMT"){
             continue;
         }
-        result +=  separ1 + '\t' + key1 + ' : [\n';
+        result +=  separ1 + '\t"' + key1 + '" : [\n';
         var separ2 = "";
-        for (let j = 0; j < myLogVar[key1].length; j++){
+        for (var j = 0; j < myLogVar[key1].length; j++){
             result += separ2 + '\t\t{\n';
             var separ3= "";
-            for (let key2 in myLogVar[key1][j]){
+            for (var key2 in myLogVar[key1][j]){
                 if(key2 == "Columns"){
                     //result += String.format(separ3 + '\t\t\t{0} : "[{1}]"', key2 , myLogVar[key1][j][key2]);
 
-                    result += separ3 + "\t\t\t" + key2 + " : [ ";
+                    result += separ3 + '\t\t\t"' + key2 + '" : [ ';
                     var separ4 = "";
-                    for (let k = 0; k < myLogVar[key1][j][key2].length; k++){
+                    for (var k = 0; k < myLogVar[key1][j][key2].length; k++){
                         result += separ4 + '"' + myLogVar[key1][j][key2][k] + '"';
                         if (separ4 == ""){
                             separ4 = ", ";
@@ -149,10 +145,10 @@ function printObj2JSON(myLogVar){
                     result += " ]";
                 }
                 else if(isNaN(Number(myLogVar[key1][j][key2]))){
-                    result += String.format(separ3 + '\t\t\t{0} : "{1}"', key2 , myLogVar[key1][j][key2]);
+                    result += String.format(separ3 + '\t\t\t"{0}" : "{1}"', key2 , myLogVar[key1][j][key2]);
                 }
                 else{
-                    result += String.format(separ3 + '\t\t\t{0} : {1}', key2 , Number(myLogVar[key1][j][key2]));
+                    result += String.format(separ3 + '\t\t\t"{0}" : {1}', key2 , Number(myLogVar[key1][j][key2]));
                 }
 
                 if (separ3 == ""){
@@ -181,14 +177,12 @@ function printObj2JSON(myLogVar){
 log2Var(myLogPath, function(tryVAr){
 
     // TODO use this var in another script
-    //console.log(tryVAr);
+    // console.log(tryVAr);
     // Make a JSON object properly spaced
-    var DataJSON = "./output-JSON/2017-02-21_17-24-06-DATA.json";
+    var DataJSON = "./output-JSON/47 11-07-2017 14-16-04.json";
     if(fs.existsSync(DataJSON)){
         fs.unlinkSync(DataJSON);
     }
     fs.appendFileSync(DataJSON, printObj2JSON(tryVAr));
 
 });
-
-
